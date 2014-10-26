118f149521f67e87e2e2af9eef799283b10b5cf4
austinberard <austinberard@outlook.com>
2014-10-26 02:13:01 -0400

    name change

---
+++ Google-Directions.py
@@ -0,0 +1,20 @@
+__author__ = 'Austin'
+from urllib.request import urlopen
+import simplejson
+
+
+def Directions(start, end, mode):
+    formattedStart = start.replace(" ", "_")
+    formattedEnd = end.replace(" ", "_")
+
+    url = "https://maps.googleapis.com/maps/api/directions/json?origin=%s&destination=%s&mode=%s&key=AIzaSyCG4JPL7D7eLCnOap0mZnc5KCjOz2WXgf0" % (formattedStart, formattedEnd, mode)
+    print(url)
+    json_obj = urlopen(url)
+    data = simplejson.load(json_obj)
+    print(data)
+    #for step in data['routes'][0]['legs'][0]['steps']:
+        # print(step['html_instructions'])
+
+
+
+Directions("42.340021,-71.100812", "woonsocket", "driving")
