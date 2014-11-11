b0e2ef47093dfa1ff28c7d9c352eb8aabaa3674e
austinberard <austinberard@outlook.com>
2014-11-11 12:18:31 -0500

    Created day and night usage plots

---
+++ Day-vs-Night.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+__author__ = 'Austin'
+
+import matplotlib.pyplot as plt
+import csv
+import gzip
+import datetime
+
+grid = []
+dayGrid = []
+nightGrid = []
+
+STATIONS = 150
+for i in range(0, STATIONS):
+    dayGrid.append([])
+    for j in range(0, STATIONS):
+        dayGrid[i].append(0)
+for i in range(0, STATIONS):
+    nightGrid.append([])
+    for j in range(0, STATIONS):
+        nightGrid[i].append(0)
+
+with gzip.open("hubway_trips.csv.gz", mode='rt') as csvfile:
+    for row in csv.reader(csvfile, delimiter=","):
+        if row[5] == "strt_statn":
+            continue
+        if row[5] == "" or row[7] == "":
+            continue
+        start = int(row[5])
+        end = int(row[7])
+
+        rawDate = row[4]
+        betterDate = rawDate.replace(" ", "/")
+        bettererDate = betterDate.replace(":", "/")
+        date = bettererDate.split("/")
+
+        hour = int(date[3])
+
+        if start > STATIONS or end > STATIONS:
+            print("Ouch "+str(start) + " " + str(end))
+            exit()
+
+        if hour in range(6, 18):
+            dayGrid[start][end] += 1
+        if hour in range(18, 24) or hour in range(0, 6):
+            nightGrid[start][end] += 1
+
+
+print(dayGrid)
+print(nightGrid)
+max = 0
+for i in range(0, STATIONS):
+    for j in range(0, STATIONS):
+        if dayGrid[i][j] > max:
+            max = dayGrid[i][j]
+        elif nightGrid[i][j] > max:
+            max = nightGrid[i][j]
+print(max)
+
+def darkness(d):
+    return str(1-(d/max))
+
+cs = []
+pts = []
+for i in range(0,STATIONS):
+    for j in range(0,STATIONS):
+        pts.append([i,j])
+        cs.append(darkness(dayGrid[i][j]))
+
+xs, ys = zip(*pts)
+
+plt.scatter(xs, ys, c = cs, s = 1000, edgecolors='none')
+plt.xlim(0, 150)
+plt.ylim(0, 150)
+plt.title("Day Usage")
+plt.show()
+
+
+
+
+cs = []
+pts = []
+for i in range(0,STATIONS):
+    for j in range(0,STATIONS):
+        pts.append([i,j])
+        cs.append(darkness(nightGrid[i][j]))
+
+xs, ys = zip(*pts)
+
+plt.scatter(xs, ys, c = cs, s = 1000, edgecolors='none')
+plt.xlim(0, 150)
+plt.ylim(0, 150)
+plt.title("Night Usage")
+plt.show()
+
+
+
