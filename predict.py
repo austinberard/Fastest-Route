a66d24520ea96640bbb8e1f458f5976f2bbd5162
austinberard <austinberard@outlook.com>
2015-05-07 20:05:07 -0400

    Added two programs, one finds how many males vs females use the service, the other keeps track ofthe age of riders, and make a dictionary out of the ages and their occurences

--- predict.py
+++ predict.py
@@ -2,11 +2,12 @@
 __author__ = 'Austin'
 
 import sys
+import csv
+from scipy.ndimage import imread
 
 from sklearn import linear_model
 from sklearn import svm
 from sklearn.ensemble import RandomForestClassifier
-
 
 def samples(file):
     samples = []
@@ -17,32 +18,33 @@ def samples(file):
             data = [int(n) for n in line.split(", ")]
             samples.append(data[:-1])
             results.append(data[-1])
-    return samples, results
+    return (samples, results)
 
 
 if __name__ == "__main__":
     training = sys.argv[1]
     held = sys.argv[2]
-    print("%s %s" % (training, held))
+    print ("%s %s" % (training, held))
 
-    train_samples, train_results = samples(training)
-    held_samples, held_results = samples(held)
 
-    print(train_samples)
-    print(train_results)
-    print(held_samples)
-    print(held_results)
+    train_samples, train_results = samples(training);
+    held_samples, held_results = samples(held);
+
+    print(train_samples);
+    print(train_results);
+    print(held_samples);
+    print(held_results);
 
     models = [
         linear_model.LinearRegression(),
-        linear_model.Lasso(alpha=0.1),
-        linear_model.LassoLars(alpha=0.1),
+        linear_model.Lasso(alpha = 0.1),
+        linear_model.LassoLars(alpha = 0.1),
         svm.SVR(),
         RandomForestClassifier(n_estimators=10)
         # FactorModel()
     ]
 
-    models.extend([linear_model.Ridge(alpha=a) for a in [0.2, 0.4, 0.6, 0.8]])
+    # models.extend([linear_model.Ridge(alpha=a) for a in [0.2, 0.4, 0.6, 0.8]])
     for model in models:
         m = model
         print("The model is:" + str(m))
